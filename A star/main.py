1dd9e26865d51ff207772877579f9d59e0557177
Mohamed Khairy <91643324+mohamedkhairy996@users.noreply.github.com>
2022-12-08 15:38:43 +0200

    Add files via upload

--- A star/main.py
+++ A star/main.py
@@ -11,69 +11,36 @@ for line in file:
 file.close()
 
 
-def aStarAlgo(start_node, stop_node):
-    open_set = set(start_node)
-    closed_set = set()
-    g = {}
-    parents = {}
-    g[start_node] = 0
-    parents[start_node] = start_node
-    while len(open_set) > 0:
-        for v in open_set:
-            if g[v] + int(heuristic(v)) <= int(heuristic(start_node)):
-                n = v
-        if n == stop_node or graph[n] == None:
-            pass
-        else:
-            for (m, weight) in get_neighbors(n):
-                if m not in open_set and m not in closed_set:
-                    open_set.add(m)
-                    parents[m] = n
-                    g[m] = g[n] + int(weight)
-                else:
-                    if g[m] > g[n] + int( weight):
-                        # update g(m)
-                        g[m] = g[n] + int(weight)
-                        parents[m] = n
-                        if m in closed_set:
-                            closed_set.remove(m)
-                            open_set.add(m)
-        if n == None:
-            print('Path does not exist!')
-            return None
-        if n == stop_node:
-            path = []
-            while parents[n] != n:
-                path.append(n)
-                n = parents[n]
-            path.append(start_node)
-            path.reverse()
-            print('A* Path found: ', path)
-            print("Path cost is = " ,g[stop_node])
+def path_f_cost(path):
+    g_cost=0
+    for(node,cost) in path:
+        g_cost += int(cost)
+    last_node= path[-1][0]
+    h_cost = h[last_node]
+    f_cost = int(h_cost) + g_cost
+    return f_cost,last_node,g_cost
+
+def a_star(graph , start , goal):
+    visited = []
+    queue = [[(start , 0)]]
+    while queue:
+        queue.sort(key=path_f_cost)
+        path = queue.pop(0)
+        node = path[-1][0]
+        if node in visited:
+            continue
+        visited.append(node)
+        if node == goal :
             return path
-        # remove n from the open_list, and add it to closed_list
-        # because all of his neighbors were inspected
-        closed_set.add(n)
-        open_set.remove(n)
-
-    print('Path does not exist!')
-
-    return None
-
-
-
-def get_neighbors(v):
-    if v in graph:
-        return graph[v]
-    else:
-        return None
-
-def heuristic(n):
-    H_dist = {}
-    H_dist = h
-    return H_dist[n]
+        else:
+            neighbour =graph.get(node , [])
+            for (node2 , cost) in neighbour:
+                new_path = path.copy()
+                new_path.append((node2,cost))
+                queue.append(new_path)
 
 
 #Function calling------------------------------
-
-aStarAlgo('A', 'J')
+path =a_star(graph,'A','J')
+print("The A* path is : ",path)
+print("The cost of this path is = ",path_f_cost(path)[2])
